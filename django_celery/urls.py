66453ab5b3fa72743ce7486218f031bb0b721547
Antônio Lima Jr <antonio.df.013@gmail.com>
2023-06-26 02:15:52 -0300

    [GEN-16] - feat: Add drf_yasg for API documentation

--- django_celery/urls.py
+++ django_celery/urls.py
@@ -1,12 +1,28 @@
 from app import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import routers
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version="v0.0.1",
+        description="Welcome",
+        terms_of_service="htt://localhost:8000/doc/",
+        contact=openapi.Contact(email="jason@jaseci.org"),
+        license=openapi.License(name="Awesome IP"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -15,6 +31,7 @@ router.register(r"groups", views.GroupViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls, name="admin"),
+    # Auth
     path(
         "api/token/",
         TokenObtainPairView.as_view(),
@@ -29,5 +46,21 @@ urlpatterns = [
         "api/token/verify/",
         TokenVerifyView.as_view(),
         name="token_verify",
+    ),
+    # Docs
+    re_path(
+        r"^doc(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "doc/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
     ),
 ]
