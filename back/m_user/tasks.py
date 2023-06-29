34979a5ee4fac707184dc28124ceed8549226049
Antônio Lima Jr <antonio.df.013@gmail.com>
2023-06-28 23:44:33 -0300

    feat: ajuste time celeryBeat

--- back/m_user/tasks.py
+++ back/m_user/tasks.py
@@ -6,11 +6,11 @@ from celery.schedules import crontab
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 1 seconds.
-    sender.add_periodic_task(1.0, test.s("hello"), name="add every 1")
-    sender.add_periodic_task(1.0, add.s(5, 10), name="add every 10")
+    sender.add_periodic_task(100, test.s("hello"), name="add every 1")
+    sender.add_periodic_task(100, add.s(5, 10), name="add every 10")
 
     # Calls test('world') every 3 seconds
-    sender.add_periodic_task(3.0, test.s("world"), expires=10)
+    sender.add_periodic_task(100, test.s("world"), expires=10)
 
 
 # Tasks
